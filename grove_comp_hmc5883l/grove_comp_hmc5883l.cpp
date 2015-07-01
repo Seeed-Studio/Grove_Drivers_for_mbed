a39014483dfb410abb3a1905aa1c3324fb170637
Jack Shao <jacky.shaoxg@gmail.com>
2015-07-01 16:07:19 +0800

    bug fix: the order of output is x z y, rather than xyz.

--- grove_comp_hmc5883l/grove_comp_hmc5883l.cpp
+++ grove_comp_hmc5883l/grove_comp_hmc5883l.cpp
@@ -11,7 +11,7 @@ GroveCompass::GroveCompass(int pinsda, int pinscl)
 bool GroveCompass::GroveCompass::write_setup(void)
 {
     uint8_t regValue = 0x00;
-	
+
 	//setScale(this->i2c, (float)1.3); // Set the scale of the compass.
 	regValue = 0x01;
 	m_Scale = 0.92;
@@ -22,7 +22,7 @@ bool GroveCompass::GroveCompass::write_setup(void)
 	cmdbuf[1] = regValue;
 	suli_i2c_write(this->i2c, HMC5883L_ADDRESS, cmdbuf, 2);
 	setMeasurementMode(this->i2c, MEASUREMENT_CONTINUOUS); // Set the measurement mode to Continuous
-	
+
     return true;
 }
 
@@ -40,14 +40,14 @@ void GroveCompass::grove_compass_getxyz_raw(I2C_T *i2c, int16_t *x, int16_t *y, 
 	suli_i2c_write(i2c, HMC5883L_ADDRESS, &cmdbuf[0], 1);
 	suli_i2c_read(i2c, HMC5883L_ADDRESS, databuf, 6);
 	*x = (databuf[0] << 8) | databuf[1];
-	*y = (databuf[2] << 8) | databuf[3];
-	*z = (databuf[4] << 8) | databuf[5];
+	*z = (databuf[2] << 8) | databuf[3];
+	*y = (databuf[4] << 8) | databuf[5];
 }
 
 bool GroveCompass::grove_compass_getxyz_scaled(I2C_T *i2c, float *cx, float *cy, float *cz)
 {
     int16_t x,y,z;
-	
+
     grove_compass_getxyz_raw(i2c, &x,&y,&z);
 	*cx = (float)x * m_Scale;
 	*cy = (float)y * m_Scale;
@@ -59,11 +59,11 @@ bool GroveCompass::grove_compass_getxyz_scaled(I2C_T *i2c, float *cx, float *cy,
 bool GroveCompass::read_compass_heading(float *heading)
 {
     float cx, cy, cz;
-	
+
 	grove_compass_getxyz_scaled(this->i2c, &cx, &cy, &cz);
-	
+
 	float head = atan2(cy, cx) - 0.0457;
-	
+
 	// Correct for when signs are reversed.
 	if(head < 0)
 	head += 2*PI;
@@ -74,7 +74,7 @@ bool GroveCompass::read_compass_heading(float *heading)
 
 	// Convert radians to degrees for readability.
 	*heading = head * 180 / PI;
-	
+
 	return true;
 }
 
